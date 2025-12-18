0e4c368fbb926ed2d30a5a0ff4919ae06dad7b4e
Notjessand <jessennorom@gmail.com>
2025-12-18 16:29:33 +0100

    x

--- next.config.mjs
+++ next.config.mjs
@@ -8,14 +8,21 @@
 
 // export default nextConfig;
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Move it here, to the top level
-  serverExternalPackages: ["ssh2"],
-  
-  // Keep experimental empty or remove it if you don't need other flags
-  experimental: {}, 
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // This tells Webpack to ignore these modules and not try to bundle them
+      config.externals.push("ssh2", "cpu-features", "nan");
+    }
+    return config;
+  },
+  // If the key above still throws a warning, you can try moving this
+  // back into experimental just to see if it clears the warning,
+  // but the webpack fix above is the "heavy duty" solution.
+  experimental: {
+    serverExternalPackages: ["ssh2"],
+  },
 };
 
 export default nextConfig;
